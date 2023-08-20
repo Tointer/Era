e4ec9107941a98f0df520ef49e79826674c9c106
ibragimov.i <Tointer@yandex.ru>
2023-08-21 00:39:14 +0800

    extract days table component + add table animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,23 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-    import Bit from './bit.svelte'
-    import { Card, Button } from 'flowbite-svelte';
+    import DayTable from './dayTable.svelte'
+    import { Card } from 'flowbite-svelte';
 
-    // let startDate = new Date(new Date().getFullYear(), 0, 1);
-    // let endDate = new Date(new Date().getFullYear(), 11, 31);
-    let startDate = new Date(2019, 1, 21);
-    let endDate = new Date(2029, 1, 21);
-    
-    let daysTotal = Math.floor((endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24);
-
+    console.log("before mount " + Date.now()/1000)
     onMount(() => {
-        const anchor = document.getElementById("today");
-        console.log(anchor);
-        anchor?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        console.log("on mount "  + Date.now()/1000)
     });
 
-    function addDays(date: Date, days: number): Date {
-        var result = new Date(date);
-        result.setDate(result.getDate() + days);
-        return result;
-    }
-
-    function getDayState(startDate: Date, offset: number): DayState{
-        let day = addDays(startDate, offset);
-        let currentDay = Math.floor(Date.now()/1000/60/60/24);
-        let dayNumber = Math.floor(day.getTime()/1000/60/60/24);
-        if(dayNumber < currentDay){
-            return DayState.Passed;
-        }
-        else if(dayNumber == currentDay){
-            return DayState.Current;
-        }
-        else{
-            return DayState.Future;
-        }
-    }
-
-    enum DayState{
-        Passed, Current, Future
-    }
 </script>
 
 <div class="flex align-center justify-center">
     <aside class="h-[90vh] w-1/5 left-0 mx-5">
         <Card class="text-center h-full" size="lg"></Card>
     </aside>
-    <div class="flex flex-col">
-        <div class="grid grid-cols-25 max-h-[90vh] no-scrollbar overflow-y-auto">
-            {#each Array(daysTotal) as _, index (index)}
-                <Bit dayState={getDayState(startDate, index)} day={addDays(startDate, index)}/> 
-            {/each}
-        </div>
-    </div>
+    <DayTable/>
     <aside class="h-[90vh] w-1/5 right-0 mx-5">
         <Card class="text-center h-full w-full " size="lg"></Card>
     </aside>
 </div>
 
-<style>
-    .no-scrollbar::-webkit-scrollbar {
-        display: none;
-    }
-</style>
 
