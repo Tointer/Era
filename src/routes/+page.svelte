5d9ba3abd8cc00ba0f9db8e1be3a6cbd0947cfe9
ibragimov.i <Tointer@yandex.ru>
2023-08-21 18:59:26 +0800

    add tag input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
     import DayTable from './dayTable.svelte'
-    import { Card } from 'flowbite-svelte';
+    import { Card, Input } from 'flowbite-svelte';
     import { selectedDay } from "$lib/dayStore";
+
+    let picker : any;
 
     console.log("before mount " + Date.now()/1000)
     onMount(() => {
@@ -18,7 +20,10 @@
     <DayTable/>
     <aside class="h-[90vh] w-1/5 right-0 mx-5">
         <Card class="text-center h-full w-full " size="lg">
-            {$selectedDay.toLocaleDateString("en-US", { weekday: "long", year: 'numeric', month: 'long', day: 'numeric' })}
+            <div class="flex flex-row justify-between content-center">
+                <Input class="w-16" type="text" placeholder="tag" />
+                {$selectedDay.toLocaleDateString("en-US", { weekday: "long", year: 'numeric', month: 'long', day: 'numeric' })}
+            </div>
         </Card>
     </aside>
 </div>
