0eeb7d67a079d8fda579393086c9be7781c37756
ibragimov.i <Tointer@yandex.ru>
2023-09-03 00:17:05 +0800

    add agenda component

---
+++ src/components/Agenda.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+    import { Card, Input } from 'flowbite-svelte';
+    import { slide } from 'svelte/transition';
+    import { quintOut } from 'svelte/easing';
+    import '../app.postcss';
+
+    const beforeMount = Date.now()/1000;
+    onMount(() => {
+        const afterMount = Date.now()/1000;
+
+        console.log("Agenda mounted in: " + (afterMount - beforeMount));
+    });
+</script>
+
+<div 
+    class="h-[90vh] w-full right-0 mx-5 max-w-2xl" 
+    transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}
+>
+    <Card class="h-full w-full max-w-full">
+        <div class="flex flex-row justify-center content-center">
+        </div>
+    </Card>
+</div>
+
+
+
+
